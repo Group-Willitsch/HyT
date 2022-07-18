aea3586d646c1cfe13dcc37beb95b8e1cfc0ea89
Pietro Vahramian <pietro.vahramian@unibas.ch>
2022-07-18 15:51:35 +0200

    added al sequences for normal mode at 10 and 12 kV

--- Simulations/zip_and_unzip_out_files.sh
+++ Simulations/zip_and_unzip_out_files.sh
@@ -12,5 +12,9 @@ for filename in ./*/output/out*.zip; do
 	unzip "${filename%.*}.zip" 		# unzip all
 done
 
+for filename in ./Stark_deceleration_with_MATLAB_parfor/sequences/*/*.zip; do
+#	echo "$filename"
+	unzip "${filename}"
+done
 # find . -name 'out*.dat' -delete 		# find, then delete all the appropriate .dat files
 
